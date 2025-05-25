63c11892a4bc98854765ee0d163fa4979dbdeba2
EmanuelOrozco <emanuelorozco2011@hotmail.com>
2025-05-25 16:49:51 -0500

    Update folder Routes in front/src

--- front/src/Routes/App.tsx
+++ front/src/Routes/App.tsx
@@ -11,25 +11,76 @@ import GestionarFormularios from '../Pages/GestionarFormularios';
 import EditarEvento from '../Pages/EditarEvento';
 import RegistroVoluntario from '../Pages/RegistroVoluntario';
 import EditarFormulario from '../Pages/EditarFormulario';
+import { AuthProvider } from '../context/AuthContext';
+import ProtectedRoute from '../Components/ProtectedRoute';
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/eventos" element={<Eventos />} />
-        <Route path="/eventos/crear" element={<CrearEventos />}/>
-        <Route path="/eventos/gestionar" element={<GestionarEventos />}/>
-        <Route path="/eventos/editar/:id" element={<EditarEvento />} />
-        <Route path="/formularios" element={<Formularios />} />
-        <Route path="/formularios/crear" element={<CrearFormularios />} />
-        <Route path="/formularios/gestionar" element={<GestionarFormularios />} />
-        <Route path="/formularios/editar/:id" element={<EditarFormulario />} />
-        <Route path="/RegistroVoluntario" element={<RegistroVoluntario />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          {/* Public routes */}
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/voluntario/registro/:id" element={<RegistroVoluntario />} />
+          
+          {/* Protected admin routes */}
+          <Route path="/dashboard" element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          } />
+          
+          <Route path="/eventos" element={
+            <ProtectedRoute>
+              <Eventos />
+            </ProtectedRoute>
+          } />
+          
+          <Route path="/eventos/crear" element={
+            <ProtectedRoute>
+              <CrearEventos />
+            </ProtectedRoute>
+          } />
+          
+          <Route path="/eventos/gestionar" element={
+            <ProtectedRoute>
+              <GestionarEventos />
+            </ProtectedRoute>
+          } />
+          
+          <Route path="/eventos/editar/:id" element={
+            <ProtectedRoute>
+              <EditarEvento />
+            </ProtectedRoute>
+          } />
+          
+          <Route path="/formularios" element={
+            <ProtectedRoute>
+              <Formularios />
+            </ProtectedRoute>
+          } />
+          
+          <Route path="/formularios/crear" element={
+            <ProtectedRoute>
+              <CrearFormularios />
+            </ProtectedRoute>
+          } />
+          
+          <Route path="/formularios/gestionar" element={
+            <ProtectedRoute>
+              <GestionarFormularios />
+            </ProtectedRoute>
+          } />
+          
+          <Route path="/formularios/editar/:id" element={
+            <ProtectedRoute>
+              <EditarFormulario />
+            </ProtectedRoute>
+          } />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 };
 
